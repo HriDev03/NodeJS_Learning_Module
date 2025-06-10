6e06199f535efd2d9b4254e5de17229aaff6c2c3
HriDev03 <hridyesh.sharma@innovasolutions.com>
2025-06-10 17:37:05 +0530

    postgres prima , mongodb prisma authN and AuthZ

---
+++ Week3/Prisma/server2/index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const cookieParser=require("cookie-parser")
+require('dotenv').config()
+const userRouter = require("./routes/userRoutes") 
+
+//regular middleware
+//middleware : something that happens in between
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// we need middleware for cookies whebever a request comes we will catch it
+app.use(cookieParser())
+
+app.use('/user',userRouter)
+
+app.get('/',(req,res)=>{
+    res.send("Default route pe aa gye")
+})
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+})
