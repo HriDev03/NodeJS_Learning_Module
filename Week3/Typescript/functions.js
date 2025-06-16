ade30c3348a1ef1cf1865cf72bcd643cbacf412b
HriDev03 <hridyesh.sharma@innovasolutions.com>
2025-06-16 17:59:34 +0530

    ts stuff

--- Week3/Typescript/functions.js
+++ Week3/Typescript/functions.js
@@ -23,15 +23,16 @@ greet2("Hridyesh", 2);
 const greet3 = (a, b) => {
     return (`Hello ${a} your empId is ${b}`);
 };
-//optional and default parameters in Ts
+//optional and default parameters in Ts , assign values to parameters itself
 const greet4 = (a, b = 1) => {
     return (`Welcome ${a} your id is ${b}`);
 };
 const emp = greet4("Hridy");
 console.log(emp);
-//optional parameters
+//optional parameters  ? after parameter
 const greet5 = (a, b) => {
     return (`Welcome ${a} your id is ${b}`);
 };
 const employee = greet5("Hridy");
 console.log(employee);
+//9 thapa
